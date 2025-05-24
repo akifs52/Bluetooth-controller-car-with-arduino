bb9fcc0d656031a9d0001317e6463572f9336354
akifs52 <akifselim02@gmail.com>
2025-05-24 17:09:53 +0300

    Update carduino.ino

--- carduino/carduino.ino
+++ carduino/carduino.ino
@@ -1,4 +1,7 @@
 char command;
+int normalSpeed = 175;
+int turningSpeed = 175;
+String inputString = "";
 
 #define solMotorPos 7
 #define solMotorNeg 8
@@ -6,69 +9,97 @@ char command;
 #define sagMotorNeg 12
 #define solMotorPWM 9
 #define sagMotorPWM 10
-#define normalSpeed 175
-#define turningSpeed 175
 
 void setup() {
-  Serial.begin(9600); // Bluetooth HC-05 9600 baud ile çalışır
+  Serial.begin(9600); // HC-05 için 9600 baud
 
   pinMode(sagMotorPWM, OUTPUT);  // ENA (PWM)
   pinMode(sagMotorPos, OUTPUT);  // IN1
   pinMode(sagMotorNeg, OUTPUT);  // IN2
-  pinMode(solMotorPos, OUTPUT); // IN3
-  pinMode(solMotorNeg, OUTPUT); // IN4
-  pinMode(solMotorPWM, OUTPUT); // ENB (PWM)
+  pinMode(solMotorPos, OUTPUT);  // IN3
+  pinMode(solMotorNeg, OUTPUT);  // IN4
+  pinMode(solMotorPWM, OUTPUT);  // ENB (PWM)
 }
 
 void loop() {
-  if (Serial.available()) {
-    command = Serial.read();
-
-    switch (command) {
-      case 'F': // ileri
-        digitalWrite(sagMotorPos, HIGH);
-        digitalWrite(sagMotorNeg, LOW);
-        digitalWrite(solMotorPos, HIGH);
-        digitalWrite(solMotorNeg, LOW);
-        analogWrite(sagMotorPWM, normalSpeed);  // Hız (0-255)
-        analogWrite(solMotorPWM, normalSpeed);
-        break;
-
-      case 'B': // geri
-        digitalWrite(sagMotorPos, LOW);
-        digitalWrite(sagMotorNeg, HIGH);
-        digitalWrite(solMotorPos, LOW);
-        digitalWrite(solMotorNeg, HIGH);
-        analogWrite(sagMotorPWM, normalSpeed);  // Hız (0-255)
-        analogWrite(solMotorPWM, normalSpeed);
-        break;
-
-      case 'R': // sola
-        digitalWrite(sagMotorPos, LOW);
-        digitalWrite(sagMotorNeg, HIGH);
-        digitalWrite(solMotorPos, HIGH);
-        digitalWrite(solMotorNeg, LOW);
-        analogWrite(sagMotorPWM, turningSpeed);  // Hız (0-255)
-        analogWrite(solMotorPWM, turningSpeed);
-        break;
-
-      case 'L': // sağa
-        digitalWrite(sagMotorPos, HIGH);
-        digitalWrite(sagMotorNeg, LOW);
-        digitalWrite(solMotorPos, LOW);
-        digitalWrite(solMotorNeg, HIGH);
-        analogWrite(sagMotorPWM, turningSpeed);  // Hız (0-255)
-        analogWrite(solMotorPWM, turningSpeed);
-        break;
-
-      case 'S': // dur
-        digitalWrite(sagMotorPos, LOW);
-        digitalWrite(sagMotorNeg, LOW);
-        digitalWrite(solMotorPos, LOW);
-        digitalWrite(solMotorNeg, LOW);
-        analogWrite(sagMotorPWM, 0);  // Hız (0-255)
-        analogWrite(solMotorPWM, 0);
-        break;
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') {
+      parseCommand(inputString);
+      inputString = "";
+    } else {
+      inputString += inChar;
     }
+  }
+}
+
+void parseCommand(String cmd) {
+  if (cmd.length() == 0) return;
+
+  char prefix = cmd.charAt(0);
+
+  if (prefix == 'F' || prefix == 'B' || prefix == 'L' || prefix == 'R' || prefix == 'S') {
+    executeMovement(prefix);
+  }
+  else if (prefix == 'N') {
+    int newSpeed = cmd.substring(1).toInt();
+    if (newSpeed >= 0 && newSpeed <= 255) {
+      normalSpeed = newSpeed;
+    }
+  }
+  else if (prefix == 'T') {
+    int newSpeed = cmd.substring(1).toInt();
+    if (newSpeed >= 0 && newSpeed <= 255) {
+      turningSpeed = newSpeed;
+    }
+  }
+}
+
+void executeMovement(char command) {
+  switch (command) {
+    case 'F': // ileri
+      digitalWrite(sagMotorPos, HIGH);
+      digitalWrite(sagMotorNeg, LOW);
+      digitalWrite(solMotorPos, HIGH);
+      digitalWrite(solMotorNeg, LOW);
+      analogWrite(sagMotorPWM, normalSpeed);
+      analogWrite(solMotorPWM, normalSpeed);
+      break;
+
+    case 'B': // geri
+      digitalWrite(sagMotorPos, LOW);
+      digitalWrite(sagMotorNeg, HIGH);
+      digitalWrite(solMotorPos, LOW);
+      digitalWrite(solMotorNeg, HIGH);
+      analogWrite(sagMotorPWM, normalSpeed);
+      analogWrite(solMotorPWM, normalSpeed);
+      break;
+
+    case 'R': // sola
+      digitalWrite(sagMotorPos, LOW);
+      digitalWrite(sagMotorNeg, HIGH);
+      digitalWrite(solMotorPos, HIGH);
+      digitalWrite(solMotorNeg, LOW);
+      analogWrite(sagMotorPWM, turningSpeed);
+      analogWrite(solMotorPWM, turningSpeed);
+      break;
+
+    case 'L': // sağa
+      digitalWrite(sagMotorPos, HIGH);
+      digitalWrite(sagMotorNeg, LOW);
+      digitalWrite(solMotorPos, LOW);
+      digitalWrite(solMotorNeg, HIGH);
+      analogWrite(sagMotorPWM, turningSpeed);
+      analogWrite(solMotorPWM, turningSpeed);
+      break;
+
+    case 'S': // dur
+      digitalWrite(sagMotorPos, LOW);
+      digitalWrite(sagMotorNeg, LOW);
+      digitalWrite(solMotorPos, LOW);
+      digitalWrite(solMotorNeg, LOW);
+      analogWrite(sagMotorPWM, 0);
+      analogWrite(solMotorPWM, 0);
+      break;
   }
 }
